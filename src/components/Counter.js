d87854f3e5fb84ddc9619d28eca5ce9b121b3ce3
unknown <anour2168@gmail.com>
2022-10-14 15:58:29 +0200

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function Counter(props) {
+  const {increament,decreament,count}= props
   return (
     <div className='content'>
-      <button onClick={props.increament}>+</button>
-      <p>{props.count}</p>
-      <button onClick={props.decreament}>-</button>
+      <button onClick={increament}>+</button>
+      <p>{count}</p>
+      <button onClick={decreament}>-</button>
     </div>
   )
 }
